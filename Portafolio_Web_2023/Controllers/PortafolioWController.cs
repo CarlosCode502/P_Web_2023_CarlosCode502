e45f6ffc5232394ed8c39ef3373bcabcff66861f
CarlosCode502 <carloshl.code21@gmail.com>
2023-09-08 17:55:37 -0600

    Trabajando en las vistas parciales y el contenido.

--- Portafolio_Web_2023/Controllers/PortafolioWController.cs
+++ Portafolio_Web_2023/Controllers/PortafolioWController.cs
@@ -14,7 +14,16 @@ namespace Portafolio_Web_2023.Controllers
 
         public IActionResult Index()
         {
-            return View();
+			//V#56 Principio de responsabilidad Única
+			//Se crea una instancia para poder acceder a la clase y a sus métodos
+			//Se comento debido a V#57 Inyección de dependencias 
+			//RepositorioProyectos repositorioProyectos = new();
+
+			//V#54 Listado de Proyectos
+			//Mostraremos los 3 proyectos más recientes.
+            
+
+			return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
