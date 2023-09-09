172d3d2e4d5db89eb64f3d6246948ce77247670f
CarlosCode502 <carloshl.code21@gmail.com>
2023-09-08 19:33:42 -0600

    Inyectando y mostrando el repositorio de proyectos.

--- Portafolio_Web_2023/Controllers/PortafolioWController.cs
+++ Portafolio_Web_2023/Controllers/PortafolioWController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio_Web_2023.Models;
+using Portafolio_Web_2023.Services;
 using System.Diagnostics;
 
 namespace Portafolio_Web_2023.Controllers
 {
     public class PortafolioWController : Controller
     {
-        private readonly ILogger<PortafolioWController> logger;
+		//V#57 Inyección de dependencias 
+		//Con la Inyección de dependencias
+		//Delagmos la instanciación de las dependencias de nuestras clases a otras clases
+		//Campo de la clase 
 
-        public PortafolioWController(ILogger<PortafolioWController> logger) {
-            this.logger = logger;
-        }
+		//Realizamos la inyección del la inerfaz del repositorio
+
+		#region Campos generados automáticamente
+		private readonly ILogger<PortafolioWController> logger;
+		private readonly IRepositorioProyecto repositorioProyecto;
+		#endregion
+
+		//V#60 Entendiendo ILoger msjs de logs(Cuando se pasa ILogger a HomeController detectara los logs en la consola)
+		//Ctrl+. y en Create and assign fiel 'logger' para crearlo como CAMPO y agregar automaticamente los valores
+		public PortafolioWController(ILogger<PortafolioWController> logger, IRepositorioProyecto repositorioProyecto) {
+			//Generados automáticamente
+			this.logger = logger;
+			this.repositorioProyecto = repositorioProyecto;
+		}
 
         public IActionResult Index()
         {
@@ -21,12 +36,35 @@ namespace Portafolio_Web_2023.Controllers
 
 			//V#54 Listado de Proyectos
 			//Mostraremos los 3 proyectos más recientes.
-            
+			var proyectos = repositorioProyecto.ObtenerListadoProyectos().Take(3).ToList();
 
-			return View();
+			//Obtenemos los 3 proyectos más recientes y los mandamos al IEnumerable (como ya recibe un modelo con los mismos datos)
+			//Mismo IEnumerable que debe pasarse a la vista para poder mostrar los datos.
+			var modelo = new PortafolioIndexViewModel
+			{
+				//Asignamos los proyectos obtenidos al IEnumerable 
+				Proyectos = proyectos
+			};
+
+			//mandamos el modelo a la vista para poder acceder y manejar los datos
+			return View(modelo);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		//V#56 Principio de responsabilidad Única
+		//AQUI ESTABA MI METODO OBTENER LISTADO
+
+		//V#62 Creando una Nueva Vista - Mostrando todos los proyectos.
+		//Controlador para la nueva vista(pag) en donde se mostrarán todos mis proyectos.
+		public IActionResult Proyectos()
+		{
+			//Pasamos todos los proyectos
+			var proyectos = repositorioProyecto.ObtenerListadoProyectos();
+
+			return View(proyectos);
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
