3e22889c7ff943b74f4a14a500ee87d26a022bc9
CarlosCode502 <carloshl.code21@gmail.com>
2023-09-08 19:01:06 -0600

    Se agregó el modelo de proyecto, además del IEnumerable para poder usarlo en la vista.
    Se creo el repositorio que contendrá los proyectos, con su Interfaz correspondiente para poder inyectarlo al proyecto.

---
+++ Portafolio_Web_2023/Models/PortafolioIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Portafolio_Web_2023.Models
+{
+	//V#54 Listado de Proyectos
+
+	/// <summary>
+	/// Esta clase va a servir para encapsular el modelo del Proyecto.
+	/// Para luego a través del modelo acceder a los datos en la vista
+	/// </summary>
+	public class PortafolioIndexViewModel
+	{
+		/// <summary>
+		/// Esta propiedad va a recibir lo relacionado al modelo ProyectoViewModel
+		/// </summary>
+		public IEnumerable<ProyectoViewModel>? Proyectos { get; set; }
+
+		////V#59 Servicios(Transient, Scoped, Singelton)
+		//public EjemploGUIDViewModel? EjemploGUID_1 { get; set; }
+		//public EjemploGUIDViewModel? EjemploGUID_2 { get; set; }
+	}
+}
---
+++ Portafolio_Web_2023/Services/IRepositorioProyecto.cs
@@ -0,0 +1,9 @@
+﻿namespace Portafolio_Web_2023.Services
+{
+	//V#58 Inyección de dependencias con Interfaces
+	//Una interfaz es como un contrato que nos obliga a implementar ciertos metodos de una clase.
+	//Una de las ventajas de utilizar inyección de dependencias es poder agregar flexibilidad en las relaciones de nuestras clases
+	public interface IRepositorioProyecto
+	{
+	}
+}
