404fb02adfb6304c5b5306aa5ced7f0f0c998524
CarlosCode502 <carloshl.code21@gmail.com>
2023-09-24 14:14:49 -0600

    Validando campos de contacto.

--- Portafolio_Web_2023/Models/ContactoViewModel.cs
+++ Portafolio_Web_2023/Models/ContactoViewModel.cs
@@ -14,11 +14,11 @@ namespace Portafolio_Web_2023.Models
         //Sirve para especificar que un String deba tener un longitud exacta o un rango de longitud
         //Y para especificar un rango Min(x) y Max(y) [StringLength(6, MinimumLength = 5)] 
         //Se personaliza el msj de error [ ErrMsj="El campo {0} debe tener
-        [Required(ErrorMessage = "El campo {0} es obligatorio. ")]
+        [Required(ErrorMessage = "* El campo {0} es obligatorio.")]
         [StringLength(150, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener una longitud miníma de {2} y máxima de {1}")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio. ")]
+        [Required(ErrorMessage = "* El campo {0} es obligatorio.")]
         [DataType(DataType.EmailAddress)]
         [DisplayName("Correo")]
         public string Email { get; set; }
@@ -26,7 +26,7 @@ namespace Portafolio_Web_2023.Models
         //V#45 Passwords y TextArea, Scaffolfing DataType(Permite especificar el tipo de dato del textbox)
         //Es muy útil cuando se desea personalizar el tipo de dato que se desea guardar en este 
         //En este caso el campo de esta propiedad pasa de ser un txt normal a un txt multilinea
-        [Required(ErrorMessage = "El campo {0} es obligatorio. ")]
+        [Required(ErrorMessage = "* El campo {0} es obligatorio.")]
         [DataType(DataType.MultilineText)]
         public string Mensaje { get; set; }
     }
